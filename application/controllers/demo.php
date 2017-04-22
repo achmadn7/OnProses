ca196fe206f7ba7ab9732717ce7992093971b44b
achmadn7 <achmad1300018083@webmail.uad.ac.id>
2017-04-22 11:01:18 +0700

    add file

---
+++ application/controllers/demo.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of demo
+ *
+ * @author nugrahantoro
+ */
+class Demo extends CI_Controller {
+    //put your code here
+    function index(){
+        $this->load->view('templateadmin');
+    }
+}
